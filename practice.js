f6c53f659b8b158fe8385f938060390578be12c1
Corbin Herrera <53804047+corbincad@users.noreply.github.com>
2019-10-27 17:00:06 -0500

    Quiz Homework

---
+++ practice.js
@@ -0,0 +1,37 @@
+const questions = [{
+
+    question : "Does JavaScript use camel notation?",
+    choiceA : "Yes",
+    choiceB : "No",
+    choiceC : "Camels scare me",
+    correct : "A",
+    correct = true,
+},
+
+{
+
+    question : "Wraping text in quotations turns the text into a ...",
+    choiceA : "Object",
+    choiceB : "Array",
+    choiceC : "String",
+    correct : "C",
+    correct = true,
+}];
+
+const startButton = document.getElementById("startButton")
+
+document.addEventListener("click", startGame)
+
+function startGame() {
+
+
+}
+
+
+function setNextQuestion() {
+
+
+}
+
+
+
